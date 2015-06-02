e0f07cc869f51e0cb6ffc75c95087a5120775719
Rich Soni <rich@richsoni.com>
2015-06-02 16:17:06 -0400

    add font awesome

--- src/rootComponent.js
+++ src/rootComponent.js
@@ -28,7 +28,7 @@ Meta = React.createClass({
     return <div>
       <div>Created By: <a style={this._style.link} href={this.props.creator_url}>{this.props.creator_display_name}</a> in {moment(this.props.created_on).format('YYYY')}</div>
       <div>{this.props.description}</div>
-      <div><a style={this._style.link} href={this.props.rss}>(TK FEED ICON){this.props.name}</a></div>
+      <div><a style={this._style.link} href={this.props.rss}><i className='fa fa-rss'></i>{this.props.name}</a></div>
       <div><a style={this._style.link} href={this._videosLink}> {this.props.total_videos} Videos</a></div>
       <div><a style={this._style.link} href={this._followersLink}> {this.props.total_subscribers} Followers</a></div>
     </div>
