9f7fb2ccf4199727e369c23d956968c09e825a81
Rich Soni <rich@richsoni.com>
2015-06-02 15:04:18 -0400

    add meta component

--- src/rootComponent.js
+++ src/rootComponent.js
@@ -1,25 +1,37 @@
 Logo = require("./logoComponent")
 
+INFO_PROPS = {
+  id:                   React.PropTypes.number,
+  name:                 React.PropTypes.string,
+  description:          React.PropTypes.string,
+  logo:                 React.PropTypes.string,
+  badge:                React.PropTypes.string,
+  url:                  React.PropTypes.string,
+  rss:                  React.PropTypes.string,
+  created_on:           React.PropTypes.string,
+  creator_id:           React.PropTypes.number,
+  creator_display_name: React.PropTypes.string,
+  creator_url:          React.PropTypes.string,
+  total_videos:         React.PropTypes.number,
+  total_subscribers:    React.PropTypes.number
+}
+
+Meta = React.createClass({
+  propTypes: INFO_PROPS,
+  render: function(){
+    return <div>
+      here
+    </div>
+  }
+})
+
 module.exports = React.createClass({
-  propTypes: {
-    id:                   React.PropTypes.number,
-    name:                 React.PropTypes.string,
-    description:          React.PropTypes.string,
-    logo:                 React.PropTypes.string,
-    badge:                React.PropTypes.string,
-    url:                  React.PropTypes.string,
-    rss:                  React.PropTypes.string,
-    created_on:           React.PropTypes.string,
-    creator_id:           React.PropTypes.number,
-    creator_display_name: React.PropTypes.string,
-    creator_url:          React.PropTypes.string,
-    total_videos:         React.PropTypes.number,
-    total_subscribers:    React.PropTypes.number
-  },
+  propTypes: INFO_PROPS,
 
   render: function(){
     return <article>
       <Logo src={this.props.logo} alt={this.props.name} href={this.props.url} />
+      <Meta {...this.props} />
     </article>
   }
 })
