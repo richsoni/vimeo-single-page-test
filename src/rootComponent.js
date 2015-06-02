49146140406b4875a2f7a90e7e10e331301ccbf7
Rich Soni <rich@richsoni.com>
2015-06-02 16:31:59 -0400

    refactor out styles

--- src/rootComponent.js
+++ src/rootComponent.js
@@ -2,18 +2,23 @@ Logo = require("./logoComponent")
 Meta = require("./metaComponent")
 C    = require("./constants")
 
+style = {
+  logo: {
+    width: '100%',
+    minHeight: 200
+  },
+  meta: {
+    maxWidth: 300
+  }
+}
+
 module.exports = React.createClass({
   propTypes: C.INFO_PROPS,
 
   render: function(){
     return <article>
-      <Logo style={{
-        width: '100%',
-        minHeight: 200
-      }} src={this.props.logo} alt={this.props.name} href={this.props.url} />
-      <Meta style={{
-        maxWidth: 300
-      }} {...this.props} />
+      <Logo style={style.logo} src={this.props.logo} alt={this.props.name} href={this.props.url} />
+      <Meta style={style.meta} {...this.props} />
     </article>
   }
 })
