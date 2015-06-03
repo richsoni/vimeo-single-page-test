a78b4dc0f3d36de1b4640d919d24bf734def7eba
Rich Soni <rich@richsoni.com>
2015-06-03 08:23:35 -0400

    add video

--- src/videosComponent.js
+++ src/videosComponent.js
@@ -1,10 +1,33 @@
 Video = React.createClass({
   propTypes: {
-    video: React.PropTypes.object
+    id:                       React.PropTypes.number,
+    title:                    React.PropTypes.string,
+    description:              React.PropTypes.string,
+    url:                      React.PropTypes.string,
+    upload_date:              React.PropTypes.string,
+    mobile_url:               React.PropTypes.string,
+    thumbnail_small:          React.PropTypes.string,
+    thumbnail_medium:         React.PropTypes.string,
+    thumbnail_large:          React.PropTypes.string,
+    user_id:                  React.PropTypes.number,
+    user_name:                React.PropTypes.string,
+    user_url:                 React.PropTypes.string,
+    user_portrait_small:      React.PropTypes.string,
+    user_portrait_medium:     React.PropTypes.string,
+    user_portrait_large:      React.PropTypes.string,
+    user_portrait_huge:       React.PropTypes.string,
+    stats_number_of_likes:    React.PropTypes.number,
+    stats_number_of_plays:    React.PropTypes.number,
+    stats_number_of_comments: React.PropTypes.number,
+    duration:                 React.PropTypes.number,
+    width:                    React.PropTypes.number,
+    height:                   React.PropTypes.number,
+    tags:                     React.PropTypes.string,
+    embed_privacy:            React.PropTypes.string
   },
 
   render: function(){
-    return <div>video</div>
+    return <div>{this.props.title}</div>
   }
 })
 
@@ -24,7 +47,7 @@ module.exports = React.createClass({
   render: function(){
     return <div>
       {this.props.videos.map(function(video){
-        return <Video video={video} />
+        return <Video {...video} />
       })}
     </div>
   }
