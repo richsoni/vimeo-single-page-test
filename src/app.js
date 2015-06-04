6b136324288577112b294b46dea447bef479baeb
Rich Soni <rich@richsoni.com>
2015-06-04 09:32:43 -0400

    make videos an immutable store

--- src/app.js
+++ src/app.js
@@ -1,21 +1,28 @@
-global.React  = require("react")
-global.Bacon  = require("baconjs")
-global.moment = require("moment")
-global._      = require("underscore")
+global.React     = require("react")
+global.Bacon     = require("baconjs")
+global.moment    = require("moment")
+global._         = require("underscore")
+global.Immutable = require("immutable")
 
 var RootComponent = require("./rootComponent")
 const SEEDS       = require("./seeds")
 
+class VideoStore {
+  constructor(data) {
+    this.__data = new Immutable.List(data)
+  }
+}
+
 class Controller {
   constructor() {
-    this.stream = Bacon.Bus()
-    this.videos = SEEDS.BD_VIDEOS
-    this.info   = SEEDS.STAFF_INFO
+    this.stream     = Bacon.Bus()
+    this.videoStore = new VideoStore(SEEDS.BD_VIDEOS)
+    this.info       = SEEDS.STAFF_INFO
     this.render()
   }
 
   render() {
-    React.render(<RootComponent {...this.info} videos={this.videos} />, document.body)
+    React.render(<RootComponent {...this.info} videos={this.videoStore.__data} />, document.body)
   }
 }
 
