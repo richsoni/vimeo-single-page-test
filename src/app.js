ab453cb4aeed0a0728a577a5461e7ac66f51061b
Rich Soni <rich@richsoni.com>
2015-06-04 08:31:13 -0400

    pull out seeds to work on controller stuff

--- src/app.js
+++ src/app.js
@@ -3,42 +3,10 @@ global.moment = require("moment")
 global._      = require("underscore")
 
 var RootComponent = require("./rootComponent")
-const BD_VIDEOS = require("./bd_videos")
-
-const STAFF_INFO = {
-    "id": 927,
-    "name": "Vimeo Staff Picks",
-    "description": "We really love videos, and these are the videos we really, really love. All of these videos have been hand picked by the real humans who work at Vimeo. We hope you enjoy them!",
-    "logo": "https://i.vimeocdn.com/channel/289181_980?mh=250",
-    "badge": "",
-    "url": "https://vimeo.com/channels/staffpicks",
-    "rss": "https://vimeo.com/channels/staffpicks/videos/rss",
-    "created_on": "2007-10-23 13:00:43",
-    "creator_id": 152184,
-    "creator_display_name": "Vimeo Staff",
-    "creator_url": "https://vimeo.com/staff",
-    "total_videos": 9290,
-    "total_subscribers": 177680
-}
-
-const BD_INFO = {
-    "id": 332146,
-    "name": "Bob Dylan",
-    "description": "",
-    "logo": "",
-    "badge": "",
-    "url": "https://vimeo.com/channels/332146",
-    "rss": "https://vimeo.com/channels/332146/videos/rss",
-    "created_on": "2012-05-15 14:27:04",
-    "creator_id": 6153775,
-    "creator_display_name": "Fabio Ramelli",
-    "creator_url": "https://vimeo.com/user6153775",
-    "total_videos": 19,
-    "total_subscribers": 12
-}
+const SEEDS = require("./seeds")
 
 function render(){
-  React.render(<RootComponent {...STAFF_INFO} videos={BD_VIDEOS} />, document.body)
+  React.render(<RootComponent {...SEEDS.STAFF_INFO} videos={SEEDS.BD_VIDEOS} />, document.body)
 }
 
 document.addEventListener("DOMContentLoaded", render);
